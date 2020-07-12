a1977c8daa07d994444df68f66babadcb805d226
Marcin Binkowski <33808280+fridgelord@users.noreply.github.com>
2020-07-12 19:27:50 +0200

    Initial commit

---
+++ platforma.py
@@ -0,0 +1,127 @@
+from time import sleep
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import pandas
+
+
+LOGIN_SITE = "https://platformaopon.pl/"
+CREDENTIALS_FILE = "credentials"
+
+PARAMS = {
+    "hankook": "52",
+    "lato": "1",
+    "zima": "2",
+    "całoroczne": "3",
+    "summer": "1",
+    "winter": "2",
+    "all-season": "3",
+    "all-weather": "3",
+    "t": "24",
+    "h": "26",
+    "v": "27",
+    "w": "28",
+    "y": "29",
+}
+
+SITE_PREFIX = "https://platformaopon.pl/buy/offers?"
+SITE_SUFFIX = "SaleOfferTyreFilterForm%5Bsearch%5D="
+
+BUY_SITE = [
+    "SaleOfferTyreFilterForm%5Bsale_offer%5D%5Bproducer%5D%5BpersonalizedProducersList%5D=",
+    "SaleOfferTyreFilterForm%5BtyreParameters%5D%5Bsize%5D=",
+    "SaleOfferTyreFilterForm%5BtyreParameters%5D%5Bseason%5D=",
+    "SaleOfferTyreFilterForm%5BtyreParameters%5D%5Bspeed%5D=",
+    "SaleOfferTyreFilterForm%5BtyreParameters%5D%5Bcapacity%5D=",
+    "SaleOfferTyreFilterForm%5Bsale_offer%5D%5Bname%5D=",
+]
+
+
+SIZES = [
+    ["Hankook", "195/65R15", "zima", "T", "91", "W452", ],
+    ["Hankook", "205/55R16", "zima", "T", "91", "W452", ],
+]
+
+
+driver = webdriver.Firefox()
+driver.get(LOGIN_SITE)
+
+with open(CREDENTIALS_FILE) as fp:
+    credentials = fp.read().splitlines()
+
+username_field = driver.find_element_by_xpath("//input[contains(@id, 'username')]")
+password_field = driver.find_element_by_xpath("//input[contains(@id, 'password')]")
+save_me_tick = driver.find_element_by_xpath("//span[contains(@class, 'tick halflings')]")
+submit_field = driver.find_element_by_xpath("//input[contains(@id, 'submit')]")
+
+username_field.send_keys(credentials[0])
+password_field.send_keys(credentials[1])
+save_me_tick.click()
+submit_field.click()
+sleep(4)
+
+results = []
+for size in SIZES:
+    site = ""
+    for key, item in enumerate(size):
+        if key != 1:
+            item = item.lower().replace("/", "%2F")
+        site += BUY_SITE[key] + PARAMS.get(item, item) + "&"
+    final_site = SITE_PREFIX + site + SITE_SUFFIX
+    driver.get(final_site)
+
+
+    offers = driver.find_elements_by_xpath("//tbody/tr")
+    for offer in offers:
+        try:
+            size = offer.find_element_by_xpath(".//td[contains(@class, 'tyre-size')]").text
+        except NoSuchElementException:
+            continue
+        pattern = offer.find_element_by_xpath(".//span[contains(@class, 'model-name')]").text
+        stock = offer.find_element_by_xpath(".//span[contains(@class, 'value')]").text
+        price_str = offer.find_element_by_xpath(".//div[contains(@class, 'big-price')]").text
+        price = float(price_str.replace(" zł", "").replace(",","."))
+        try:
+            seller = offer.find_element_by_xpath(".//img[contains(@class, 'company-logo')]").get_attribute("title")
+        except NoSuchElementException:
+            seller = offer.find_element_by_xpath(".//div[contains(@class, 'company-table-row__name')]").text
+        delivery = offer.find_element_by_xpath(".//div[contains(@class, 'delivery-time delivery-time-info')]").text
+        try:
+            dot = offer.find_element_by_xpath(".//div[contains(@class, 'tyre-year')]").text
+        except NoSuchElementException:
+            dot = ""
+        try:
+            remarks = offer.find_element_by_xpath(".//div[contains(@class, 'description')]").text
+        except NoSuchElementException:
+            remarks = ""
+
+        results.append([
+            size,
+            pattern,
+            seller,
+            price,
+            stock,
+            dot,
+            remarks,
+        ])
+    sleep(8)
+
+print(results)
+driver.find_element_by_xpath("//a[contains(@title, 'Wyloguj')]").click()
+
+df = pandas.DataFrame(results,
+                 columns=[
+                     "size",
+                     "pattern",
+                     "seller",
+                     "price",
+                     "stock",
+                     "dot",
+                     "remarks",
+                 ])
+
+df.to_excel("data.xlsx")
+
+
+
+
+
