159bfd1d66be5431fdf61aa22d50ba7b26ba9e4d
Marcin Binkowski <33808280+fridgelord@users.noreply.github.com>
2020-07-19 19:01:20 +0200

    select sizes from external file
    select sizes from external csv files with fallback to default sizes from script. minor fixes

--- platforma.py
+++ platforma.py
@@ -5,13 +5,15 @@ import pandas
 
 
 LOGIN_SITE = "https://platformaopon.pl/"
-CREDENTIALS_FILE = "credentials"
+CREDENTIALS_FILE = "credentials.txt"
+SIZES_FILE = "sizes.csv"
 
 PARAMS = {
     "hankook": "52",
     "lato": "1",
     "zima": "2",
     "całoroczne": "3",
+    "wielosezon": "3",
     "summer": "1",
     "winter": "2",
     "all-season": "3",
@@ -30,16 +32,16 @@ BUY_SITE = [
     "SaleOfferTyreFilterForm%5Bsale_offer%5D%5Bproducer%5D%5BpersonalizedProducersList%5D=",
     "SaleOfferTyreFilterForm%5BtyreParameters%5D%5Bsize%5D=",
     "SaleOfferTyreFilterForm%5BtyreParameters%5D%5Bseason%5D=",
-    "SaleOfferTyreFilterForm%5BtyreParameters%5D%5Bspeed%5D=",
     "SaleOfferTyreFilterForm%5BtyreParameters%5D%5Bcapacity%5D=",
+    "SaleOfferTyreFilterForm%5BtyreParameters%5D%5Bspeed%5D=",
     "SaleOfferTyreFilterForm%5Bsale_offer%5D%5Bname%5D=",
     "SaleOfferTyreFilterForm%5Bsale_offer%5D%5Bamount%5D%5Bmin%5D=20&",
 ]
 
 
-SIZES = [
-    ["Hankook", "195/65R15", "zima", "T", "91", "W452", "20",],
-    ["Hankook", "205/55R16", "zima", "T", "91", "W452", "20",],
+DEFAULT_SIZES = [
+    ["Hankook", "195/65R15", "zima", "91", "T", "W452", "20",],
+    ["Hankook", "205/55R16", "zima", "91", "T", "W452", "20",],
 ]
 
 
@@ -48,6 +50,12 @@ driver.get(LOGIN_SITE)
 
 with open(CREDENTIALS_FILE) as fp:
     credentials = fp.read().splitlines()
+
+try:
+    with open(SIZES_FILE) as fp:
+        sizes = [line.split(";") for line in fp.read().splitlines()[1:]]
+except FileNotFoundError:
+    sizes = DEFAULT_SIZES
 
 username_field = driver.find_element_by_xpath("//input[contains(@id, 'username')]")
 password_field = driver.find_element_by_xpath("//input[contains(@id, 'password')]")
@@ -61,7 +69,7 @@ submit_field.click()
 sleep(4)
 
 results = []
-for size in SIZES:
+for size in sizes:
     site = ""
     for key, item in enumerate(size):
         if key != 1:
@@ -108,6 +116,7 @@ for size in SIZES:
 
 print(results)
 driver.find_element_by_xpath("//a[contains(@title, 'Wyloguj')]").click()
+driver.close()
 
 df = pandas.DataFrame(results,
                  columns=[
