397fd6895a9efcc4ddbb25e061d8c0acc61f0862
Marcin Bińkowski <binkowski@hankooktire.com.pl>
2020-10-03 17:25:24 +0200

    correct behavior when destination file of append to excel is open in windows

--- DataFrameAppend.py
+++ DataFrameAppend.py
@@ -1,5 +1,5 @@
 import pandas
-import os
+import shutil
 from openpyxl import load_workbook
 from datetime import datetime
 from pathlib import Path
@@ -34,56 +34,55 @@ class DataFrameAppend(pandas.DataFrame):
         Returns: None
         """
 
-        # ignore [engine] parameter if it was passed
-        if 'engine' in to_excel_kwargs:
-            to_excel_kwargs.pop('engine')
-        if 'header' in to_excel_kwargs:
-            header = to_excel_kwargs['header']
-            to_excel_kwargs.pop('header')
-        else:
-            header = True
-
-        if not os.access(filename, os.W_OK):
-            path = Path(filename)
-            directory = path.parent
-            file = path.stem
-            new_file = file + "_" + datetime.now().strftime("%Y%m%d_%H%M%S")
-            suffix = path.suffix
-            new_file_path = Path(directory).joinpath(new_file + suffix)
-            os.copy(filename, str(new_file_path))
-            filename = str(new_file_path)
-
-        with pandas.ExcelWriter(filename, engine='openpyxl') as writer:
-            try:
-                writer.book = load_workbook(filename)
-
-                # get the last row in the existing Excel sheet
-                # if it was not specified explicitly
-                if startrow is None and sheet_name in writer.book.sheetnames:
-                    startrow = writer.book[sheet_name].max_row
-
-                # truncate sheet
-                if truncate_sheet and sheet_name in writer.book.sheetnames:
-                    # index of [sheet_name] sheet
-                    idx = writer.book.sheetnames.index(sheet_name)
-                    # remove [sheet_name]
-                    writer.book.remove(writer.book.worksheets[idx])
-                    # create an empty sheet [sheet_name] using old index
-                    writer.book.create_sheet(sheet_name, idx)
-
-                # copy existing sheets
-                writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
-            except FileNotFoundError:
-                # file does not exist yet, we will create it
-                pass
-
-            if startrow is None:
-                startrow = 0
+        try:
+            # ignore [engine] parameter if it was passed
+            if 'engine' in to_excel_kwargs:
+                to_excel_kwargs.pop('engine')
+            if 'header' in to_excel_kwargs:
+                header = to_excel_kwargs['header']
+                to_excel_kwargs.pop('header')
             else:
-                header = False
+                header = True
 
-            self.to_excel(writer, sheet_name, startrow=startrow, header=header, **to_excel_kwargs)
+            with pandas.ExcelWriter(filename, engine='openpyxl') as writer:
+                try:
+                    writer.book = load_workbook(filename)
 
+                    # get the last row in the existing Excel sheet
+                    # if it was not specified explicitly
+                    if startrow is None and sheet_name in writer.book.sheetnames:
+                        startrow = writer.book[sheet_name].max_row
 
+                    # truncate sheet
+                    if truncate_sheet and sheet_name in writer.book.sheetnames:
+                        # index of [sheet_name] sheet
+                        idx = writer.book.sheetnames.index(sheet_name)
+                        # remove [sheet_name]
+                        writer.book.remove(writer.book.worksheets[idx])
+                        # create an empty sheet [sheet_name] using old index
+                        writer.book.create_sheet(sheet_name, idx)
 
+                    # copy existing sheets
+                    writer.sheets = {ws.title:ws for ws in writer.book.worksheets}
+                except FileNotFoundError:
+                    # file does not exist yet, we will create it
+                    pass
 
+                if startrow is None:
+                    startrow = 0
+                else:
+                    header = False
+
+                self.to_excel(writer, sheet_name, startrow=startrow, header=header, **to_excel_kwargs)
+        except PermissionError:
+            old_file = Path(filename)
+            root = old_file.root
+            stem = old_file.stem
+            suffix = old_file.suffix
+            new_filename = stem + "_" + datetime.now().strftime("%Y%m%d_%H%M%S") + suffix
+            new_file = Path(root).joinpath(new_filename)
+            new_filepath = str(new_file)
+            shutil.copy(filename, new_filepath)
+
+            self.append_to_excel(new_filepath, sheet_name, startrow,
+                       truncate_sheet, **to_excel_kwargs)
